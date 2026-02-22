5da9b3a7bb5c7e15b033ff6bd1aa5c50d787f4e1
zyron42 <awesomedawson42@gmail.com>
2026-02-22 09:57:18 -0500

    GenieSceneRedone

---
+++ Assets/Scripts/GenieDialogManager.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using UnityEngine;
+
+public class GenieDialogManager : MonoBehaviour
+{
+    //this is the text object
+    public TMP_Text text;
+    public TMP_Text nameText;
+    public GameObject DialogSystem;
+
+    string[] messages;
+    int messagesIndex = 0;
+    bool isActive = false;
+    // Update is called once per frame
+    public void ShowMessage(string Message)
+    {
+        messages = Message.Split('|');
+        DialogSystem.SetActive(true);
+        messagesIndex = 0;
+        isActive = true;
+        text.text = messages[messagesIndex].Trim();
+        messagesIndex = 1;
+
+
+    }
+
+    public void Skip()
+    {
+        if (!isActive)
+        {
+            return;
+        }
+
+        if (messagesIndex < messages.Length)
+        {
+
+            text.text = messages[messagesIndex].Trim();
+            messagesIndex++;
+        }
+        else
+        {
+            DialogSystem.SetActive(false);
+            messagesIndex = 0;
+            isActive = false;
+        }
+    }
+}
